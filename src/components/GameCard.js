0b1e9feb34f7cad840c71050f7cdf48a640e7bf3
n1ghtman199 <kileer3d@gmail.com>
2018-06-14 22:09:12 +0700

    Make component reusable with props

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-const GameCard = () => (
+const GameCard = ({ game }) => (
     <div className="ui card">
         <div className="image">
-            <span className="green ui ribbon label">$32.99</span>
-            <img src="https://cf.geekdo-images.com/BMUcxCZM_AikQ7uXeuDg43RZIWo=/fit-in/246x300/pic2840020.jpg" alt="Game Cover"/>
+            <span className="green ui ribbon label">${game.price}</span>
+            <img src={game.thumbnail} alt="Game Cover"/>
         </div>
         <div className="content">
-            <a href="#" className="header">Quadropolis</a>
+            <a href="#" className="header">{game.name}</a>
             <div className="meta">
-                <i className="icon users" />2-4 &nbsp;
-                <i className="icon wait" />60 min.
+                <i className="icon users" />{game.players} &nbsp;
+                <i className="icon wait" />{game.duration} min.
             </div>
         </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,35 @@ import GameCard from "./components/GameCard";
 
 import 'semantic-ui-css/semantic.min.css';
 
+const games = [
+{
+    name: "Quadropolis",
+    thumbnail: "https://cf.geekdo-images.com/BMUcxCZM_AikQ7uXeuDg43RZIWo=/fit-in/246x300/pic2840020.jpg",
+    price: 32.99,
+    players: '2-4',
+    duration: 60
+},
+{
+    name: "Quadropolis 2",
+    thumbnail: "https://cf.geekdo-images.com/BMUcxCZM_AikQ7uXeuDg43RZIWo=/fit-in/246x300/pic2840020.jpg",
+    price: 32.99,
+    players: '2-4',
+    duration: 60
+},
+{
+    name: "Quadropolis 3",
+    thumbnail: "https://cf.geekdo-images.com/BMUcxCZM_AikQ7uXeuDg43RZIWo=/fit-in/246x300/pic2840020.jpg",
+    price: 32.99,
+    players: '2-4',
+    duration: 60
+},
+{
+    name: "Quadropolis 4",
+    thumbnail: "https://cf.geekdo-images.com/BMUcxCZM_AikQ7uXeuDg43RZIWo=/fit-in/246x300/pic2840020.jpg",
+    price: 32.99,
+    players: '2-4',
+    duration: 60
+},
+];
 
-
-render(<GameCard/>, document.getElementById('root'));
+render(<GameCard game={games[1]}/>, document.getElementById('root'));
