dea231da09fcdeec324da6369dd53bc3035f3518
n1ghtman199 <kileer3d@gmail.com>
2018-06-14 22:30:56 +0700

    add PropTypes

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import GameCard from './GameCard';
+import PropTypes from 'prop-types';
 
 const GamesList = ({ games }) => (
     <div className="ui four cards">
         {games.map(game => <GameCard game={game} key={game._id} />)}
     </div>
 );
+
+GamesList.propTypes = {
+    games: PropTypes.arrayOf(PropTypes.object).isRequired
+};
 
 export default GamesList;
